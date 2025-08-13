99020f427f102972a8459cfe3f90a283929ff64f
Hallan Christian <hallancma@gmail.com>
2025-08-13 08:10:03 -0300

    atualizando repo

--- 02-Desenvolvimento Web Front-end/NIVEL-01-FUNDAMENTOS/02-estrutura-da-aplicacao/01-projeto-feed/projeto-feed/src/App.jsx
+++ 02-Desenvolvimento Web Front-end/NIVEL-01-FUNDAMENTOS/02-estrutura-da-aplicacao/01-projeto-feed/projeto-feed/src/App.jsx
@@ -1,14 +1,21 @@
-// JSX - Javascript + XML
 import { Header } from "./components/Header";
-import { Post } from "./Post";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
 import "./global.css";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post author="Hallan Christian" content="lorem ipsum" />
-      <Post author="Jonatas" content="Um novo Post muito legal" />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Hallan Christian" content="lorem ipsum" />
+          <Post author="Jonatas" content="Um novo Post muito legal" />
+        </main>
+      </div>
     </div>
   );
 }
