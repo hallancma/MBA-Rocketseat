71028a3787c8caf410ee7862eaf0f690d12cb343
Hallan Christian <hallancma@gmail.com>
2025-09-09 08:10:46 -0300

    Atualizando repo

---
+++ 02-Desenvolvimento Web Front-end/NIVEL-2-Aprofundando em Hooks/react-aprofundando-em-hooks-1/02-ignite-timer/projeto/src/pages/Home/index.tsx
@@ -0,0 +1,77 @@
+import { Play } from 'phosphor-react';
+import { useForm } from 'react-hook-form';
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles';
+
+type FormInputs = {
+  task: string;
+  minutesAmount: number;
+};
+
+export function Home() {
+  const { register, handleSubmit, watch } = useForm<FormInputs>({
+    defaultValues: { task: '', minutesAmount: 0 },
+  });
+
+  function handleCreateNewCycle(data: FormInputs) {
+    console.log(data);
+  }
+
+  const task = watch('task');
+  const isSubmitDisabled = !task;
+
+  return (
+    <HomeContainer>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Banana" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
+          />
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  );
+}
--- 02-Desenvolvimento Web Front-end/NIVEL-2-Aprofundando em Hooks/react-aprofundando-em-hooks-1/02-ignite-timer/src/pages/History.tsx
+++
@@ -1,7 +0,0 @@
-export function History() {
-  return (
-    <div>
-      <h1>Welcome to the History Page</h1>
-    </div>
-  );
-}
